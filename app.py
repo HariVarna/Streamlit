5dd1d1cdeb5f48aaadb5010b01e93ba59b8f4356
Jeremiah-Jefry <g.jeremiahjefry06@gmail.com>
2026-01-24 10:57:05 +0530

    Fix import typo and add student data table to Streamlit app

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import steamlit as st
+import streamlit as st
 
 st.title("My First Streamlit App")
 st.write("Hello Streamlit")
@@ -9,3 +9,10 @@ age = st.number_input("Enter your age", min_value=1)
 if st.button("Submit"):
     st.write("Name:", name)
     st.write("Age:", age)
+
+students = [
+    {"name": "Arun", "age": 19},
+    {"name": "Vinu", "age": 20}
+]
+
+st.table(students)
