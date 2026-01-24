0ea6e1a2314081265a8bc1475309b35f01ff5dbb
Jeremiah-Jefry <g.jeremiahjefry06@gmail.com>
2026-01-24 10:54:16 +0530

    Add user input fields for name and age with submission functionality

--- app.py
+++ app.py
@@ -2,3 +2,10 @@ import steamlit as st
 
 st.title("My First Streamlit App")
 st.write("Hello Streamlit")
+
+name = st.text_input("Enter your name")
+age = st.number_input("Enter your age", min_value=1)
+
+if st.button("Submit"):
+    st.write("Name:", name)
+    st.write("Age:", age)
